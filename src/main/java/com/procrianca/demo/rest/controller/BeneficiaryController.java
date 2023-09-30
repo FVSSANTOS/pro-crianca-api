4ebf35b71aa029a7c0f39c2205eb52920c128444
gabrielle-1 <gabriellerodriguesmd@gmail.com>
2023-09-30 16:20:42 -0300

    Create CRUD Beneficiary

--- src/main/java/com/procrianca/demo/rest/controller/BeneficiaryController.java
+++ src/main/java/com/procrianca/demo/rest/controller/BeneficiaryController.java
@@ -1,18 +1,20 @@
 package com.procrianca.demo.rest.controller;
 
-import com.procrianca.demo.domain.entity.User;
+import com.procrianca.demo.domain.dtos.TokenDTO;
+import com.procrianca.demo.domain.response.AuthResponse;
+import com.procrianca.demo.domain.response.HttpStatusCode;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import com.procrianca.demo.domain.entity.Beneficiary;
 import com.procrianca.demo.service.impl.BeneficaryService;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -40,16 +42,46 @@ public class BeneficiaryController {
     @PostMapping("/beneficiaries")
     @ResponseStatus(HttpStatus.CREATED)
     public Beneficiary saveBeneficiary(@RequestBody @Valid Beneficiary beneficiary){
-        log.info(beneficiary.toString());
         log.info("Calling endpoint to save a beneficiary in controller: " + log.getName());
 
         // Chame o método saveBeneficiary com o objeto passado no corpo da solicitação
         var benefiarySaved = beneficiaryService.saveBeneficiary(beneficiary);
-
         return benefiarySaved;
     }
 
+    @Operation(summary = "Update a beneficary")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Beneficiary updated",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Beneficiary.class)))
+    })
+    @PutMapping("/beneficiaries/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<AuthResponse> updateBeneficiary(@PathVariable(value = "id") @NotNull @Positive Integer id,
+                                                          @RequestBody @Valid Beneficiary beneficiary){
+        log.info("Calling endpoint to update a beneficiary in controller: " + log.getName());
 
+        var benefiaryUpdated = this.beneficiaryService.update(id, beneficiary);
+        if (benefiaryUpdated == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new AuthResponse("Beneficiário não encontrado.", HttpStatusCode.UNAUTHORIZED.getValue()) {});
+        }
+        return ResponseEntity.ok(new AuthResponse("Beneficiário atualizado com sucesso", HttpStatusCode.OK.getValue()));
+    }
+
+    @Operation(summary = "Delete beneficiary by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Beneficiary deleted"),
+            @ApiResponse(responseCode = "404", description = "Beneficiary not found")
+    })
+        @DeleteMapping("/beneficiaries/{id}")
+    public ResponseEntity<AuthResponse> deleteBeneficiary(@PathVariable(value = "id") @NotNull @Positive Integer id) {
+        log.info("Calling endpoint to delete one product in controller: " + log.getName());
+        var beneficiary = this.beneficiaryService.deleteBeneficiary(id);
+        if (!beneficiary) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new AuthResponse("Beneficiário não encontrado. Não foi possivel exlcuir.", HttpStatusCode.UNAUTHORIZED.getValue()) {});
+        }
+        return ResponseEntity.ok(new AuthResponse("Beneficiário excluído com sucesso", HttpStatusCode.OK.getValue()));
+    }
     @GetMapping("/beneficiaries")
     @ResponseStatus
     public ResponseEntity<List<Beneficiary>> getAllBeneficiaries() {
--- src/main/java/com/procrianca/demo/service/impl/BeneficaryService.java
+++ src/main/java/com/procrianca/demo/service/impl/BeneficaryService.java
@@ -1,5 +1,6 @@
 package com.procrianca.demo.service.impl;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,17 +15,38 @@ import java.util.List;
 public class BeneficaryService {
 
     @Autowired
-    private BeneficiaryRespository respository;
+    private BeneficiaryRespository repository;
 
     @Transactional
     public Beneficiary saveBeneficiary(Beneficiary beneficiary){
         if(beneficiary == null)
             throw new NullPointerException();
         
-        return respository.save(beneficiary);
+        return repository.save(beneficiary);
     }
 
     public List<Beneficiary> listAllBeneficiaries(){
-        return respository.findAll();
+        return repository.findAll();
+    }
+
+    public Beneficiary update(Integer id, Beneficiary beneficiaryUpdate) {
+        var beneficiary = this.repository.findById(id);
+
+        if (beneficiary.isEmpty()) return null;
+
+        var beneficiaryModel = beneficiary.get();
+
+        BeanUtils.copyProperties(beneficiaryUpdate, beneficiaryModel);
+        var beneficiarySaved = this.repository.save(beneficiaryModel);
+        return beneficiarySaved;
+    }
+
+    public boolean deleteBeneficiary(Integer id) {
+        var beneficiary = this.repository.findById(id);
+        if (beneficiary.isEmpty()) {
+            return false;
+        }
+        this.repository.delete(beneficiary.get());
+        return true;
     }
 }
